db02333631cf994675f53588b4f1b64144e6ea5e
DIEGOHORVATTI <d.horvattid@gmail.com>
2025-05-26 02:28:15 -0300

    Refactor App component: integrate SplashScreen for improved loading experience; update Suspense handling around AuthGuard and NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import SnackbarProvider from '@/contexts/snackbar-provider'
@@ -7,6 +8,7 @@ import AuthProvider from '@/contexts/auth-provider'
 import NavBar from '@/layouts/navbar'
 import AuthGuard from '@/guards/auth-guard'
 import MotionLazyProvider from '@/components/animate/motion-lazy-provider'
+import SplashScreen from './components/splash-screen'
 
 import { ThemeProvider } from '@/theme'
 
@@ -29,15 +31,17 @@ export default function App() {
           <SnackbarProvider>
             <AuthProvider>
               <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-                <AuthGuard>
-                  <NavBar navConfig={navConfig}>
-                    <Routes>
-                      {extractRoutes(navConfig).map(({ path, element }) => (
-                        <Route key={path} path={path} element={element} />
-                      ))}
-                    </Routes>
-                  </NavBar>
-                </AuthGuard>
+                <Suspense fallback={<SplashScreen />}>
+                  <AuthGuard>
+                    <NavBar navConfig={navConfig}>
+                      <Routes>
+                        {extractRoutes(navConfig).map(({ path, element }) => (
+                          <Route key={path} path={path} element={element} />
+                        ))}
+                      </Routes>
+                    </NavBar>
+                  </AuthGuard>
+                </Suspense>
               </BrowserRouter>
             </AuthProvider>
           </SnackbarProvider>
